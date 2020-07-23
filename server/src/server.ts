a5f4904ed24ab6a194dd6d58d739f0545a1a65c7
Luiz Felipe <raziel_libertino@hotmail.com>
2020-07-23 11:40:39 -0400

    finished chat real time

--- server/src/server.ts
+++ server/src/server.ts
@@ -5,9 +5,24 @@ const server = app.listen(3333, () => console.log("It's work!"));
 
 import io from 'socket.io';
 
-const socket = io(server);
 
-socket.on('connection', (socket) => {
-    socket.on('chat', data => console.log(data));
+interface User {
+    id: number;
+    name: string;
+    message: string;
+}
+const messages: User[] = [];
+
+const ioServer = io(server);
+
+ioServer.on('connection', (socket) => {
+    ioServer.emit('chat_history', messages);
+    
+    console.log('Alguém se conectou no chat')
+    socket.on('chat', data => {
+        const id = socket.id
+        messages.push({id, ...data});
+        ioServer.emit('chat_update', messages)
+    });
 });
 
